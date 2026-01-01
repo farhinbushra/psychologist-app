fd2f4d6e68a88671cf03a06f269f05e1075b21d0
Farhin Tabassum Bushra <tabassumbushra335@gmail.com>
2026-01-02 01:27:45 +0600

    Add files via upload

---
+++ Management.js
@@ -0,0 +1,10 @@
+function Management() {
+  return (
+    <div className="management">
+      <h2>Management</h2>
+      <p>Manage appointments & therapists</p>
+    </div>
+  );
+}
+
+export default Management;
