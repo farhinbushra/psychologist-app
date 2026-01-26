c6a494e6b14d521fcc6f39d8ec43a465159442ae
Farhin Tabassum Bushra <tabassumbushra335@gmail.com>
2026-01-27 02:02:45 +0600

    Update Management.js

--- src/components/Management.js
+++ src/components/Management.js
@@ -1,1 +1,11 @@
 
+function Management() {
+  return (
+    <div className="management">
+      <h2>Management</h2>
+      <p>Manage appointments & therapists</p>
+    </div>
+  );
+}
+
+export default Management;
