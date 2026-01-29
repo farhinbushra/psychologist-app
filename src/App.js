97b09dd29f454a66e672f992441cb1d55846f7ee
Farhin Tabassum Bushra <tabassumbushra335@gmail.com>
2026-01-30 03:15:37 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import PaymentSuccess from "./pages/PaymentSuccessFixed";
+import PaymentSuccess from "./pages/PaymentSuccess";
 import Therapist from "./components/Therapist";
 import Testimonial from"./components/Testimonial";
 import Appointment from "./components/Appointment";
